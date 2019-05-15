143d134541ac3282b799e25db72ed54c0dc74e51
Bansari <bansari2601@gmail.com>
2019-05-15 14:24:59 -0700

    code to display graph is added to the modified file'

--- LinearRegressionModel_using_scikit_lib.py
+++ LinearRegressionModel_using_scikit_lib.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[6]:
+# In[8]:
 
 get_ipython().magic('matplotlib inline')
 import numpy as np
@@ -26,19 +26,26 @@ from sklearn.metrics import mean_squared_error
 X = X.reshape((m, 1))
 
 # Creating a Model
-reg = LinearRegression()
+regressor = LinearRegression()
 
 # Fitting traing data
-reg = reg.fit(X, Y)
+regressor = regressor.fit(X, Y)
 
 # Y Prediction
-Y_pred = reg.predict(X)
+Y_pred = regressor.predict(X)
 
 # R2 score calculation
-r2_score = reg.score(X, Y)
+r2_score = regressor.score(X, Y)
+
+plt.plot(X, regressor.predict(X), color='#58b970', label='Regression Line (Predicted data)')
+plt.scatter(X, Y, c='#ef5423', label='Scatter Plot (Actual data)')
+plt.xlabel('Height')
+plt.ylabel('Weight')
+plt.legend()
+plt.show()
 
 print('R2: ' + str(r2_score))
-print('If person\'s height is 166cm, predicted weight of that person will be: ' + str(reg.predict(166)))
+print('If person\'s height is 166cm, predicted weight of that person will be: ' + str(regressor.predict(166)))
 
 
 # In[ ]:
